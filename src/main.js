ddf2a91c4d907e06b996eccebeec5be149fe5c1d
maryssecapili <56245731+maryssecapili@users.noreply.github.com>
2019-10-07 18:59:56 +0800

    Boilerplate

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// js code
+document.getElementById("js-btn").addEventListener("click", () => {
+  alert("JS compiles!!");
+});
+
+// jquery code
+$(document).ready(function() {
+  $("#jquery-btn").click(function() {
+    alert("jQuery Works.");
+  });
+});
