48776cb89d31a7f348ddac4fe767785fa14a3533
maryssecapili <56245731+maryssecapili@users.noreply.github.com>
2019-10-09 16:15:59 +0800

    Tables, Schools and Packages Section
    SVG also added.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
 // js code
-document.getElementById("js-btn").addEventListener("click", () => {
-  alert("JS compiles!!");
-});
+// document.getElementById("js-btn").addEventListener("click", () => {
+//   alert("JS compiles!!");
+// });
 
 // jquery code
-$(document).ready(function() {
-  $("#jquery-btn").click(function() {
+jQuery(document).ready(function() {
+  jQuery("#jquery-btn").click(function() {
     alert("jQuery Works.");
   });
+
+  jQuery('.school-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3
+  });
+  
+  
+  
 });
