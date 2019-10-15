a27597123cf8c3484d4bc00417e8927405d459d2
maryssecapili <56245731+maryssecapili@users.noreply.github.com>
2019-10-15 20:08:27 +0800

    October 15, 2019 Changes

--- src/main.js
+++ src/main.js
@@ -5,16 +5,76 @@
 
 // jquery code
 jQuery(document).ready(function() {
-  jQuery("#jquery-btn").click(function() {
-    alert("jQuery Works.");
+
+  // Gallery Lightbox Init
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+      event.preventDefault();
+      $(this).ekkoLightbox();
   });
 
-  jQuery('.facilities-slider').slick({
-    infinite: false,
+  // Facilities Slider Init
+  $('.facilities-slider').slick({
+    infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: true
+        }
+      },
+
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false,
+          arrows: true
+        }
+      },
+      
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+    if ($(window).width() > 767)
+    {
+      if (scroll > 50) {
+        $(".fixed-top").css("background" , "#e60019");
+      }
   
-  
+      else{
+        $(".fixed-top").css("background" , "rgba(0, 0, 0, 0.33)");  	
+      }
+    }
+
+    if ($(this).scrollTop() > 50) {
+      $('.back-to-top').fadeIn();
+    } else {
+      $('.back-to-top').fadeOut();
+    }
+  });
+
+  $('.back-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 400);
+    return false;
+  });
   
 });
